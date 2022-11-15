42310489ec6d86c01248e2932d984d9bc106d59d
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2022-11-15 14:44:49 +0100

    added a few examples for htmlforms and started work on javascript getting-started

---
+++ javascript-beginner/index.js
@@ -0,0 +1,28 @@
+let username = "Tobias Wehrle";
+const alter = 24;
+//var x = 10;
+
+console.log(username, alter);
+
+
+/* var x = 1;
+
+console.log(x);
+
+if (x === 1) {
+  var x = 2;
+  console.log(x);
+}
+
+console.log(x); */
+  
+
+// Ich bin ein ein-zeilen comment // strg+shift+p
+
+/*
+  Ich bin ein multi-line-comment ^^
+
+  lorem ipsum
+
+  undso...
+*/
